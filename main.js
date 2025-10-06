863887a7f7130f38c8d6e2a5a2fb7a67ca0a1161
IsabelaTeixeiraRodrigues <isabela.rodrigues@sptech.school>
2025-10-05 21:45:48 -0300

    API conectando no banco na vm

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3300;
 
 // habilita ou desabilita a inserção de dados no banco de dados
-const HABILITAR_OPERACAO_INSERIR = false;
+const HABILITAR_OPERACAO_INSERIR = true;
 
 // função para comunicação serial
 const serial = async (
@@ -19,11 +19,11 @@ const serial = async (
     // conexão com o banco de dados MySQL
     let poolBancoDados = mysql.createPool(
         {
-            host: 'localhost',
-            user: 'root',
-            password: 'Mywtty135790',
-            database: 'teste',
-            port: 3306
+            host: '127.0.0.1',
+            user: 'aluno',
+            password: 'Sptech#2024',
+            database: 'projeto1',
+            port: 3307
         }
     ).promise();
 
@@ -64,7 +64,7 @@ const serial = async (
 
             // este insert irá inserir os dados na tabela "medida"
             await poolBancoDados.execute(
-                'INSERT INTO sensor VALUES (DEFAULT, DEFAULT, 3, 4, ?)',
+                'INSERT INTO sensor VALUES (DEFAULT, ?)',
                 [sensorDigital]
             );
             console.log("valores inseridos no banco: " + sensorDigital);
