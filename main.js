34f51dad39fe237f1d5017d5c6cdd4c8e1d005f2
JoaoStipanic <joao.salmeida@sptech.school>
2025-10-14 22:10:39 -0300

    Arrumando a tabela q sera inserido os dados

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ const serial = async (
 
             // este insert irá inserir os dados na tabela "medida"
             await poolBancoDados.execute(
-                'INSERT INTO sensor_leitura (leitura) VALUES (?)',
+                'INSERT INTO sensor_leitura (leitura, dataLeitura, fkSensor) VALUES (?, DEFAULT, 1)',
                 [sensorDigital]
             );
             console.log("valores inseridos no banco: " + sensorDigital);
